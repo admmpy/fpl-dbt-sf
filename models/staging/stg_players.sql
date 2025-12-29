b53b2f263b81427545aa9ef6f7a5f1963e2f0e00
Admm <modernstar256@gmail.com>
2025-12-29 22:56:17 +0000

    added columns and some testing to sources.yml. Will add freshness testing next

--- models/staging/stg_players.sql
+++ models/staging/stg_players.sql
@@ -6,13 +6,13 @@ WITH source AS (
 
 cleaned AS (
     SELECT
-        FIRST_NAME                                      AS first_name,
-        SECOND_NAME                                     AS second_name,
-        WEB_NAME                                        AS web_name,  -- this is the name used on the FPL website and most straightforward display name
-
         PLAYER_ID                                       AS player_id,
         POSITION_ID                                     AS position_id,
         TEAM_ID                                         AS team_id,
+
+        FIRST_NAME                                      AS first_name,
+        SECOND_NAME                                     AS second_name,
+        WEB_NAME                                        AS web_name,  -- this is the name used on the FPL website and most straightforward display name
 
         NOW_COST                                        AS now_cost,
         MINUTES                                         AS total_minutes,
@@ -24,8 +24,6 @@ cleaned AS (
         COALESCE(CHANCE_OF_PLAYING_NEXT_ROUND, 0)       AS next_gameweek_odds,
         TOTAL_POINTS                                    AS total_points,
         FORM                                            AS form,
-        VALUE_FORM                                      AS value_form,
-        VALUE_SEASON                                    AS value_season,
         SELECTED_BY_PERCENT                             AS selected_by_percent,
         CASE 
             WHEN STATUS = 'a' 
@@ -43,9 +41,9 @@ cleaned AS (
             ELSE STATUS
         END                                             AS status,
         
-        GAMEWEEK_FETCHED                                AS gameweek,
+        GAMEWEEK_FETCHED                                AS current_gameweek,
         INGESTION_TIMESTAMP                             AS ingestion_at,
-        
+
     FROM source
 )
 
