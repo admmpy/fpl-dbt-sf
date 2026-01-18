7d36a90073d9130fcbb21a10577ca71f6cbb6f61
Admm <modernstar256@gmail.com>
2026-01-18 14:29:29 +0000

    feat: enhance fct_ml_player_features model by integrating player snapshots and adding descriptions for new fields in YAML, including form, status, and now_cost.

--- models/marts/ml/fct_ml_player_features.sql
+++ models/marts/ml/fct_ml_player_features.sql
@@ -9,6 +9,11 @@ WITH player_base AS (
     FROM {{ ref('fct_players_gameweek') }}
 ),
 
+player_snapshots AS (
+    SELECT *
+    FROM {{ ref('stg_players_gameweek_snapshot') }}
+),
+
 player_rolling AS (
     SELECT *
     FROM {{ ref('int_player_rolling_stats') }}
@@ -29,7 +34,40 @@ team_positions AS (
 
 final AS (
     SELECT 
-        pb.*,
+        pb.player_gameweek_key,
+        pb.web_name,
+        pb.player_id,
+        pb.gameweek_id,
+        pb.fixture_id,
+        pb.team_id,
+        pb.opponent_team_id,
+        pb.total_points,
+        pb.was_home,
+        pb.minutes_played,
+        pb.goals_scored,
+        pb.expected_goals,
+        pb.expected_goal_involvements,
+        pb.assists,
+        pb.expected_assists,
+        pb.clean_sheets,
+        pb.goals_conceded,
+        pb.expected_goals_conceded,
+        pb.yellow_cards,
+        pb.red_cards,
+        pb.saves,
+        pb.bonus,
+        pb.influence,
+        pb.creativity,
+        pb.threat,
+        pb.ict_index,
+        pb.opponent_defence_strength,
+        pb.team_attack_strength,
+
+        -- player snapshots
+        ps.form,
+        ps.status,
+        ps.now_cost,
+        ps.ingestion_at,
 
         -- player rolling stats
         pr.three_week_players_roll_avg_points,
@@ -49,8 +87,8 @@ final AS (
         -- team positions 
         tp.team_position                                        AS team_position,
         opt.team_position                                       AS opponent_team_position,
-        tp.team_position - opt.team_position                    AS team_position_difference -- the higer the number the better the opposition is
-    
+        tp.team_position - opt.team_position                    AS team_position_difference, -- the higer the number the better the opposition is
+
     FROM player_base                        AS pb
          LEFT JOIN player_rolling           AS pr ON pb.player_id = pr.player_id
                                                      AND pb.gameweek_id = pr.gameweek_id
@@ -61,6 +99,9 @@ final AS (
 
          LEFT JOIN team_positions           AS tp ON pb.team_id = tp.team_id
          LEFT JOIN team_positions           AS opt ON pb.opponent_team_id = opt.team_id
+
+         LEFT JOIN player_snapshots         AS ps ON pb.player_id = ps.player_id
+                                                  AND pb.gameweek_id = ps.gameweek_id
 
 )
 
