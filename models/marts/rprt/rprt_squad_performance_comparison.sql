a33c3e5955314b9f1cd0ed1deb3f8d9fe2583848
Admm <modernstar256@gmail.com>
2026-01-19 18:28:01 +0000

    - Cleaned up the dbt_project.yml by removing specific materialization settings - added rprt models with testing

---
+++ models/marts/rprt/rprt_squad_performance_comparison.sql
@@ -0,0 +1,77 @@
+/*
+Denormalized view of the current week's recommended squad
+Joins with dim_players, dim_teams, dim_positions for full context
+Pre-calculated squad totals (cost, expected points)
+Optimized for dashboard display
+*/
+
+{{
+    config(
+        materialized='table',
+        tags=['reporting']
+    )
+}}
+
+WITH base AS (
+    SELECT *
+    FROM {{ ref('fct_model_analysis') }}
+),
+
+players AS (
+    SELECT
+        player_id,
+        web_name,
+        first_name,
+        second_name
+
+    FROM {{ ref('dim_players') }}
+),
+
+teams AS (
+    SELECT
+        team_id,
+        team_name,
+        short_name
+
+    FROM {{ ref('dim_teams') }}
+),
+
+positions AS (
+    SELECT
+        position_id,
+        position_name
+
+    FROM {{ ref('dim_positions') }}
+),
+
+final AS (
+    SELECT
+        bs.recommended_at,
+        bs.gameweek_id,
+        bs.player_id,
+        pl.web_name,
+        pl.first_name,
+        pl.second_name,
+        bs.position_id,
+        ps.position_name,
+        bs.team_id,
+        tm.team_name,
+        tm.short_name                      AS team_short_name,
+        bs.now_cost,
+        bs.predicted_points,
+        bs.actual_points,
+        bs.absolute_error,
+        bs.error_bias,
+        bs.is_in_squad,
+        bs.is_starter,
+        bs.is_captain,
+        bs.is_vice_captain
+
+    FROM base                AS bs
+         LEFT JOIN players   AS pl ON bs.player_id = pl.player_id
+         LEFT JOIN teams     AS tm ON bs.team_id = tm.team_id
+         LEFT JOIN positions AS ps ON bs.position_id = ps.position_id
+)
+
+SELECT *
+FROM final
